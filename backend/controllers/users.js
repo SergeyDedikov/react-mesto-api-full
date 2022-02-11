60bef95ff5ac8086cfe459b14a15f808da1ace9f
SergeyDedikov <serded59@yandex.ru>
2022-02-11 14:02:13 +0500

    feat: add params to set coockie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -102,6 +102,8 @@ const login = (req, res, next) => {
         // вернём куку с токеном
         return res
           .cookie("jwt", token, {
+            domain: 'mesto.coolplaces.nomoredomains.work',
+            path: '/*',
             httpOnly: true,
             secure: true,
             sameSite: true,
