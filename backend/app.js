7ff45f08aa8f2bfdac8861ce558f5b47787b9de3
SergeyDedikov <serded59@yandex.ru>
2022-02-12 14:53:34 +0500

    feat: delete lib helmet

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const helmet = require("helmet");
 
 const usersRoutes = require("./routes/users");
 const cardsRoutes = require("./routes/cards");
@@ -12,7 +11,7 @@ const errorHandler = require("./middlewares/error-handler");
 const NotFoundError = require("./errors/not-found-error");
 const auth = require("./middlewares/auth");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-// const corsHandler = require("./middlewares/cors-handler");
+const corsHandler = require("./middlewares/cors-handler");
 const limiter = require("./middlewares/rate-limit");
 
 const app = express();
@@ -22,8 +21,7 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
-// app.use(corsHandler); // обработаем CORS-запросы
-app.use(helmet);
+app.use(corsHandler); // обработаем CORS-запросы
 
 // -- Auths routes
 app.post("/signin", login);
